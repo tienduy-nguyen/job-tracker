8c7fb8141d91cd4c42464bcc0c8afb99fbb8dcf0
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-09-25 01:51:21 +0200

    Fix error export

--- middleware/checkObjectId.js
+++ middleware/checkObjectId.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
-// Middleware to check for a valid object id
-const checkObjectid = (id) => (req, res, next) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params[id]))
+// middleware to check for a valid object id
+const checkObjectId = (idToCheck) => (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params[idToCheck]))
     return res.status(400).json({ msg: 'Invalid ID' });
   next();
 };
+
+module.exports = checkObjectId;
